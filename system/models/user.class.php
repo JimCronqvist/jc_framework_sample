57f24b7741001f8316dc3cc450831ba9646358fb
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-08-10 11:01:26 +0700

    Changed the properties for the example user class

--- system/models/user.class.php
+++ system/models/user.class.php
@@ -6,10 +6,10 @@ use Jc\User\jc_user;
 
 class user extends jc_user
 {
-    protected static $_users_table = 'users';
     protected static $_users_username = 'mail_address';
     protected static $_users_password = 'password';
 
+    protected $table = 'jc_users'; // Change to "users" if you want your own users table.
     protected $autoIncrementColumn = 'userid';
 
 }
